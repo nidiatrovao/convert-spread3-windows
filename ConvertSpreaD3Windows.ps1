37bf7ea1fab0a28594cef011863caa5f9a6fcf62
Nídia Trovão <13024751+nidiatrovao@users.noreply.github.com>
2021-10-07 18:17:16 -0400

    Add files via upload

---
+++ ConvertSpreaD3Windows.ps1
@@ -0,0 +1,12 @@
+Get-Content -Encoding utf8 main.js | Select -First 149 | Out-file main-new.js -Encoding utf8
+echo "json = " | Out-file main-new.js -Append -NoNewline -Encoding utf8
+echo ";" | Out-file data.json -Append -NoNewline -Encoding utf8
+Get-Content -Encoding utf8 data.json | Out-file main-new.js -Append -Encoding utf8
+Get-Content -Encoding utf8 main.js | Select -Skip 155 | Out-file main-temp.js -Encoding utf8
+Get-Content -Encoding utf8 main-temp.js | Select -First 106 | Out-file main-new.js -Append -Encoding utf8
+Get-Content -Encoding utf8 main-temp.js | Select -Skip 108 | Out-file main-new.js -Append -Encoding utf8
+Get-Content -Encoding utf8 index.html | Foreach-Object {$_ -replace 'main.js','main-new.js'} | Out-file index-new.html -Encoding utf8
+Remove-Item data.json
+Remove-Item main-temp.js
+Remove-Item main.js
+Remove-Item index.html
